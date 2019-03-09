c8390c92edc683af2662d0683519cf3ba0f6e914
trexguo <t.rex.guo@gmail.com>
2019-03-09 23:03:22 +0800

    feat: comment'

--- lib/products.dart
+++ lib/products.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products; // When props change, replace it not manipulate
 
-  Products(this.products);
+  Products(this.products) {
+    print('[Products Widget] Constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('[Products Widget] build()');
     return Column(
       children: products
           .map((element) => Card(
